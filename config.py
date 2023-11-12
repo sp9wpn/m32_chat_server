1290d803cfba7c2197db23f0c7c110d0c8f65ac5
8cH9azbsFifZ <g@ziegenhain.com>
2023-11-12 14:29:58 +0100

    moved to repo chatserver-mopp-udp

--- config.py
+++
@@ -1,9 +0,0 @@
-# Configuration for UDP connections
-SERVER_IP = "10.101.101.14" #"0.0.0.0"
-UDP_PORT = 7373
-
-# Configuration for Chat Server
-CLIENT_TIMEOUT = 3000
-MAX_CLIENTS = 100
-KEEPALIVE = 10
-CHAT_WPM = 20
